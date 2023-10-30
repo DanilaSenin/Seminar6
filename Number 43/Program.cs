b1d82745a546d2e1d8b5149f750941817f8412b8
Danila Senin <16502711@mail.ru>
2023-10-30 15:50:54 +0300

    Номер 43

---
+++ Number 43/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите k1, b1, k2, b2: ");
+decimal k1 = decimal.Parse(Console.ReadLine());
+decimal b1 = decimal.Parse(Console.ReadLine());
+decimal k2 = decimal.Parse(Console.ReadLine());
+decimal b2 = decimal.Parse(Console.ReadLine());
+decimal x = (b2 - b1)/(k1-k2);
+decimal y = k1*x+b1;
+Console.Write($"Координаты точки ({x}, {y})");
